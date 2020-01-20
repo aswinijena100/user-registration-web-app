358d576282d8c411093022e84618ea64819a90d4
prasannaKumari1 <prasannak@boston-technology.com>
2020-01-20 16:52:32 +0530

    loacation details

--- src/app/site-coordinator/locations/location-details/location-details.component.ts
+++ src/app/site-coordinator/locations/location-details/location-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-location-details',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationDetailsComponent implements OnInit {
 
-  constructor() { }
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) {}
+ 
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 
-  ngOnInit() {
+  ngOnInit() { 
+
   }
 
 }
