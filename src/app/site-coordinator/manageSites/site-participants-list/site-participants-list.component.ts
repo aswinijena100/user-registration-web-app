fbf22190eb5aa1ceead1e41f09baa7e5f6da2eb8
prasannaKumari1 <prasannak@boston-technology.com>
2020-01-21 18:16:41 +0530

    21st jan site participation list

--- src/app/site-coordinator/manageSites/site-participants-list/site-participants-list.component.ts
+++ src/app/site-coordinator/manageSites/site-participants-list/site-participants-list.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SiteParticipantsListComponent implements OnInit {
 
-  constructor() { }
+  public data : any;
+
+  constructor() { 
+    this.data = [
+      {'name':'Anil','email':'anil.singh581@gmail.com', 'age' :'34', 'city':'Noida, UP, India' },
+      {'name':'Anil', 'email' :'anil.singh581@gmail.com', 'age' :'34', 'city':'Noida' },
+      {'name':'Sunil', 'email' :'anil.singh581@gmail.com', 'age' :'34', 'city':'Noida' },
+      {'name':'Alok', 'email' :'anil.singh581@gmail.com', 'age' :'34', 'city':'Noida' },
+      {'name':'Tinku', 'email' :'anil.singh581@gmail.com', 'age' :'34', 'city':'Noida' },
+      {'name':'XYZ', 'email' :'anil.singh581@gmail.com', 'age' :'34', 'city':'Noida' },
+      {'name':'asas', 'email' :'anil.singh581@gmail.com', 'age' :'34', 'city':'Noida' },
+      {'name':'erer', 'email' :'anil.singh581@gmail.com', 'age' :'34', 'city':'Noida' },
+      {'name':'jhjh', 'email' :'anil.singh581@gmail.com', 'age' :'34', 'city':'Noida' }
+     ];
+  }
 
   ngOnInit() {
+  }
+
+   myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
   }
 
 }
