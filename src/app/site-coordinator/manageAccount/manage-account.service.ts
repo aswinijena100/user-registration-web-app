38f274c698756de4bd9eb88164031d652291a992
suresh_TE <suresht@boston-technology.com>
2020-02-27 14:21:53 +0530

    Profile changes and updated api services with validation

--- src/app/site-coordinator/manageAccount/manage-account.service.ts
+++ src/app/site-coordinator/manageAccount/manage-account.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { DataService } from "../../service/dataService";
+import { User } from "../../entity/user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManageAccountService {
+  user: User;
+  headers: {};
+  constructor(private dataService: DataService) { }
 
-  constructor() { }
+  getProfileData() {
+    //this.headers = { "userId": "kR2g5m2pJPP0P31-WNFYK8Al7jBP0mJ-cTSFJJHJ4DewuCg" };
+    let authToken = "kR2g5m2pJPP0P31-WNFYK8Al7jBP0mJ-cTSFJJHJ4DewuCg";
+    return this.dataService.HttpGetRequest('users/kR2g5m2pJPP0P31-WNFYK8Al7jBP0mJ-cTSFJJHJ4DewuCg', '');
+   // return this.dataService.HttpGetRequest('users/'+authToken, JSON.stringify(this.headers));
+  }
+
+
+  generateAuthHeader() {
+
+    // var authKey = window.localStorage.getItem("authKey");
+    // var userId = parseInt(window.localStorage.getItem("userId"));
+    // console.log(authKey)
+    // console.log(userId)
+
+    // this.headers = { "authKey": authKey, "userId": userId };
+    this.headers = { "userId": "10" };
+
+  }
+
+
 }
--- src/app/site-coordinator/manageAccount/user-profile/user-profile.component.ts
+++ src/app/site-coordinator/manageAccount/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ManageAccountService } from "../manage-account.service";
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from '../../../entity/user';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
+  user: User = new User();
+  profileAutoCompleteData: any = {};
+  errorMessage: string = '';
+  successMessage: String = '';
 
-  constructor() { }
+  @ViewChild('profile',{ static: false }) form: any;
+
+    constructor(private manageAccountService: ManageAccountService, private route: ActivatedRoute,) { }
 
   ngOnInit() {
+    this.getProfileDetails();
+  }
+
+  getProfileDetails() {
+    this.manageAccountService.getProfileData().subscribe(
+      data => {
+        console.log(data);
+        this.profileAutoCompleteData = data;
+        this.user = this.profileAutoCompleteData;
+        console.log(this.user);
+      }, error => {
+        this.errorMessage = "Falied to get Profile data";
+      });
+
+  }
+  updateProfile(){
+   
   }
 
 }
